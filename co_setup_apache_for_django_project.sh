66df760a4de4241f7e1ed2cc62318bcd80c7407e
Fabien Toune <github@lapin-blanc.com>
2014-02-14 20:14:47 +0100

    Added webdav for remote working

--- co_setup_apache_for_django_project.sh
+++ co_setup_apache_for_django_project.sh
@@ -14,34 +14,54 @@ else
 fi
 
 # Active les virtual hosts
-if grep -q "ServerName $VIRTUAL_HOST_DOMAIN" /etc/httpd/conf/httpd.conf
-then 
-    echo "Virtual host $VIRTUAL_HOST_DOMAIN already configured"
-else 
-    echo "
-    <VirtualHost *:80>
+# if grep -q "ServerName $VIRTUAL_HOST_DOMAIN" /etc/httpd/conf/httpd.conf
+# then 
+#    echo "Virtual host $VIRTUAL_HOST_DOMAIN already configured"
+# else 
+mkdir -p /home/webdav
+chown apache:apache /home/webdav
+ln -s /home/$USERNAME/ /home/webdav/$USERNAME
 
-        ServerAdmin webmaster@$VIRTUAL_HOST_DOMAIN
-        ServerName $VIRTUAL_HOST_DOMAIN
-        ErrorLog logs/$VIRTUAL_HOST_DOMAIN-error
-        CustomLog logs/$VIRTUAL_HOST_DOMAIN-access common
+echo "
+<VirtualHost *:80>
 
-        Alias /static/ /home/$USER_NAME/django/$PROJECT_NAME/static/
-        <Directory /home/$USER_NAME/django/$PROJECT_NAME/static/>
+    ServerAdmin webmaster@$VIRTUAL_HOST_DOMAIN
+    ServerName $VIRTUAL_HOST_DOMAIN
+    ErrorLog logs/$VIRTUAL_HOST_DOMAIN-error
+    CustomLog logs/$VIRTUAL_HOST_DOMAIN-access common
+
+    Alias /webdav/ /home/webdav/$USERNAME/
+    Alias /webdav /home/webdav/$USERNAME/
+    <Directory /home/webdav/$USERNAME/>
+        Dav On
+        AuthType Digest
+        AuthName \"WebDAV\"
+        AuthUserFile /etc/httpd/conf/webdav.users.pwd
+
+        Options +Indexes
+        IndexOptions FancyIndexing
+        AddDefaultCharset UTF-8
+        Require user $USERNAME
+        Order allow,deny
+        Allow from all
+    </Directory>
+
+    Alias /static/ /home/$USER_NAME/django/$PROJECT_NAME/static/
+    <Directory /home/$USER_NAME/django/$PROJECT_NAME/static/>
+        Order deny,allow
+        Allow from all
+    </Directory>
+
+    WSGIProcessGroup $VIRTUAL_HOST_DOMAIN
+    WSGIDaemonProcess $VIRTUAL_HOST_DOMAIN user=$USER_NAME group=$USER_NAME python-path=/home/$USER_NAME/django/$PROJECT_NAME/:/home/$USER_NAME/.virtualenvs/$VIRTUAL_ENV/lib/python2.7/site-packages/
+    WSGIScriptAlias / /home/$USER_NAME/django/$PROJECT_NAME/$PROJECT_NAME/wsgi.py
+
+    <Directory /home/$USER_NAME/django/$PROJECT_NAME/>
+        <Files wsgi.py>
             Order deny,allow
             Allow from all
-        </Directory>
-
-        WSGIProcessGroup $VIRTUAL_HOST_DOMAIN
-        WSGIDaemonProcess $VIRTUAL_HOST_DOMAIN user=$USER_NAME group=$USER_NAME python-path=/home/$USER_NAME/django/$PROJECT_NAME/:/home/$USER_NAME/.virtualenvs/$VIRTUAL_ENV/lib/python2.7/site-packages/
-        WSGIScriptAlias / /home/$USER_NAME/django/$PROJECT_NAME/$PROJECT_NAME/wsgi.py
-
-        <Directory /home/$USER_NAME/django/$PROJECT_NAME/>
-            <Files wsgi.py>
-                Order deny,allow
-                Allow from all
-            </Files>
-        </Directory>
-    </VirtualHost>" >> /etc/httpd/conf/httpd.conf
-fi
+        </Files>
+    </Directory>
+</VirtualHost>" > /etc/httpd/conf.d/$VIRTUAL_HOST_DOMAIN.conf
+#fi
 

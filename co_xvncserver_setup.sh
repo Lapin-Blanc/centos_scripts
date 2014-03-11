a0e29bb2110a60c57e1475419844eff912f88d3d
Fabien Toune <github@lapin-blanc.com>
2014-03-11 11:31:46 +0100

    Update co_xvncserver_setup.sh

--- co_xvncserver_setup.sh
+++ co_xvncserver_setup.sh
@@ -1,5 +1,5 @@
 yum -y groupinstall Desktop
-yum -y install tigervnc-server
+yum -y install tigervnc-server tigervnc-server-applet
 yum -y install xorg-x11-fonts-Type1 terminus-fonts terminus-fonts-console  urw-fonts mplayer-fonts xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi liberation-fonts-common liberation-sans-fonts
 
 chkconfig vncserver on
@@ -11,3 +11,4 @@ vim /etc/sysconfig/vncservers
 VNCSERVERS="1:user"
 VNCSERVERARGS[2]="-geometry 1280x800"
 # open firewall port 5901
+-A INPUT -m state --state NEW -m tcp -p tcp -m multiport --dports 5801,5901 -j ACCEPT

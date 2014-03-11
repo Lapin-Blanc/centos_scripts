54a8b069b6c79ecd2e8d4f67be1a69e0019b331f
Fabien Toune <github@lapin-blanc.com>
2014-03-11 11:05:54 +0100

    Update co_xvncserver_setup.sh

--- co_xvncserver_setup.sh
+++ co_xvncserver_setup.sh
@@ -1,6 +1,7 @@
 yum -y groupinstall Desktop
 yum -y install tigervnc-server
-yum -y install xorg-x11-fonts-Type1 terminus-fonts terminus-fonts-console
+yum -y install xorg-x11-fonts-Type1 terminus-fonts terminus-fonts-console  urw-fonts mplayer-fonts xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi liberation-fonts-common liberation-sans-fonts
+
 chkconfig vncserver on
 # as user : 
 vncpasswd
